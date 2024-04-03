4248bf794ca969e09ff8b86397804de60bb6dfbf
jinx <jinx@dist-1lab>
2024-04-03 16:35:21 +0530

    lab commit

--- app/src/main/java/com/example/music8027/MainActivity.java
+++ app/src/main/java/com/example/music8027/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.music8027;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ImageButton buttonPlaying = (ImageButton) findViewById(R.id.playing);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        buttonPlaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Playing = new Intent(view.getContext(), MainPlay.class);
+                view.getContext().startActivity(Playing);
+            }
         });
     }
 }
