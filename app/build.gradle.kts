11aea4f8c5d81c36061fbce22fc4029fa93a53a7
jinx <jinx@wsl.manjaro.loq>
2024-04-16 19:43:42 +0530

    fix splash animation color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.music8027"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
