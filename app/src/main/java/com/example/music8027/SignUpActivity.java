428c4976a90503a3dad1997ba8a9af9d96b07203
jinx <jinx@wsl.manjaro.loq>
2024-05-18 22:14:57 +0530

    fix: textview type password

--- app/src/main/java/com/example/music8027/SignUpActivity.java
+++ app/src/main/java/com/example/music8027/SignUpActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -158,5 +159,13 @@ public class SignUpActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
